01d30140280111c4b19983f45e2c7ebf8d2f2891
KouichiYamada <k-yamada@tg-inet.co.jp>
2013-07-02 13:23:30 +0900

    sample file read method modify at train

--- src/org/geese/ci/classifier/util/TrainUtil.java
+++ src/org/geese/ci/classifier/util/TrainUtil.java
@@ -1,6 +1,6 @@
 package org.geese.ci.classifier.util;
 
-import org.geese.util.Logging;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -8,6 +8,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import org.geese.util.Logging;
 import org.geese.ci.classifier.Classifier;
 import org.geese.ci.classifier.TrainException;
 
@@ -26,13 +27,15 @@ public class TrainUtil {
 	public static void train(Classifier cl, String categoryName, String fileName, Charset charset)
 		throws TrainException {
 		Path path = FileSystems.getDefault().getPath(fileName);
-		try {
-			byte[] bytes = Files.readAllBytes(path);
-			String sentense = new String(bytes, charset);
-			cl.train(sentense, categoryName);
-		} catch (IOException ex) {
-			Logging.error(ex);
-			throw new TrainException(ex.getMessage());
+
+		String sentense = "";
+		try (BufferedReader reader = Files.newBufferedReader(path, charset);) {
+			while ((sentense = reader.readLine()) != null) {
+				cl.train(sentense, categoryName); /* All lines are regarded belong in same category */
+			}
+		} catch (IOException ioe) {
+			Logging.error(ioe);
+			throw new TrainException("Failed training by '" + sentense + "' because of " + ioe.getMessage());
 		}
 	}
 
