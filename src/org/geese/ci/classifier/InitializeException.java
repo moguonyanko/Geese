0fb2893d3497086c523fd49d1b93a55c3a5c1b22
KouichiYamada <k-yamada@tg-inet.co.jp>
2013-07-04 13:08:43 +0900

    change InitializeException to abstract class

--- src/org/geese/ci/classifier/InitializeException.java
+++ src/org/geese/ci/classifier/InitializeException.java
@@ -1,6 +1,6 @@
 package org.geese.ci.classifier;
 
-public class InitializeException extends Exception{
+public abstract class InitializeException extends Exception{
 
 	public InitializeException(Throwable cause) {
 		super(cause);
