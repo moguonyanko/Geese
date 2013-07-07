8ddef18b1a49adbf8bec47e7801766d74a2518e6
moguonyanko <moguo_50000_nyanko@yahoo.co.jp>
2013-07-08 01:18:51 +0900

    default locale file modify

--- src/org/geese/config/bundle/Localization.java
+++ src/org/geese/config/bundle/Localization.java
@@ -1,20 +1,23 @@
 package org.geese.config.bundle;
 
-import java.util.ListResourceBundle;
+import org.geese.util.Logging;
 
 /**
  * Default localization class.
  * 
+ * This is default localization class file 
+ * that is used when cannot found requested locale
+ * properties.
+ * Default locale "en_US" at this library.
  * 
  * @author moguonyanko
  */
-public class Localization extends ListResourceBundle {
+public class Localization extends Localization_en_US {
 
 	@Override
 	protected Object[][] getContents() {
-
-		return new Object[][]{
-			{"error.start", "Failed at connection to classifier database."}
-		};
+		Logging.warn("Cannot found requested bundle file and use default that.");
+		
+		return super.getContents();
 	}
 }
